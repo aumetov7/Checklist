4def4b1f5931dbcbe09fcc90ca37736b0c79b22a
aumetov7 <aumetov7@gmail.com>
2022-08-04 18:21:35 +0600

    Working on cells and data

---
+++ Checklist/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklist
+//
+//  Created by Акбар Уметов on 4/8/22.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
